b7497de8a9f0aa841c849423f36e292eb68b91e2
Jamshed Khan <90842589+jamedkh@users.noreply.github.com>
2025-11-28 18:24:08 +0500

    chapters added

--- 04-card-props-drilling/src/App.jsx
+++ 04-card-props-drilling/src/App.jsx
@@ -5,7 +5,7 @@ import Heading from "./components/heading";
 const App = () => {
   return (
     <>
-      <Heading heading="React Js - Props" />
+      <Heading heading="React Js - 04 - Cards Props Drilling" />
       <Card
         img="https://images.unsplash.com/photo-1721331762508-13fe0743499f?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         title="Mona Lisa"
