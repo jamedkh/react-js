a9a7994cc594029819dea674af755ec1898ffc9f
Jamshed Khan <90842589+jamedkh@users.noreply.github.com>
2025-11-28 19:56:47 +0500

    chapters added

--- 04-card-props-drilling/src/App.jsx
+++ 04-card-props-drilling/src/App.jsx
@@ -6,27 +6,17 @@ const App = () => {
   return (
     <>
       <Heading heading="React Js - 04 - Cards Props Drilling" />
-      <Card
-        img="https://images.unsplash.com/photo-1721331762508-13fe0743499f?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        title="Mona Lisa"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quod fugit porro sapiente animi officiis!"
-        actions={
-          <a href="void" className="btn">
-            Read More
-          </a>
-        }
-      />
 
-      <Card
-        img="https://images.unsplash.com/photo-1659018966820-de07c94e0d01?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        title="AI Bot"
-        description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora aliquid ipsam maiores id laboriosam dolor laudantium ipsum quaerat neque iste."
-        actions={
-          <a href="void" className="btn">
-            Read More
-          </a>
-        }
-      />
+      <div className="profile-container">
+        <Card
+          img="https://images.unsplash.com/photo-1721331762508-13fe0743499f?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          name="Mona Lisa"
+          designation=""
+          location=""
+          email=""
+          phone=""
+        />
+      </div>
     </>
   );
 };
--- 04-card-props-drilling/src/components/card.jsx
+++ 04-card-props-drilling/src/components/card.jsx
@@ -1,18 +1,53 @@
 import React from "react";
+import "../profile-card.css";
 
-const Card = (blogCard) => {
-  console.log(blogCard);
+const Card = (profileCard) => {
+  console.log(profileCard);
 
   return (
-    <div className="card">
-      <img src={blogCard.img} alt={blogCard.title} />
+    <>
+      <div className="profile-card">
+        {/* <div className="header">
+          <button className="menuButton">⋯</button>
+        </div> */}
 
-      <h2>{blogCard.title}</h2>
+        <div className="imageWrapper">
+          <img
+            src={profileCard.img}
+            alt="Addison James profile"
+            className="profileImage"
+          />
+        </div>
 
-      <p>{blogCard.description}</p>
+        <div className="content">
+          <h1 className="name">{profileCard.name}</h1>
+          <span className="badge">{profileCard.designation}</span>
+        </div>
 
-      {blogCard.actions}
-    </div>
+        <div className="contactSection">
+          <div className="contactItem">
+            <div className="contactText">
+              <span className="label">Location:</span>
+              <span className="value">{profileCard.location}</span>
+            </div>
+          </div>
+
+          <div className="contactItem">
+            <div className="contactText">
+              <span className="label">Email:</span>
+              <span className="value">{profileCard.email}</span>
+            </div>
+          </div>
+
+          <div className="contactItem">
+            <div className="contactText">
+              <span className="label">Phone:</span>
+              <span className="value">{profileCard.phone}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
