80956907cc254386e3aca14179b39a3a5c15c922
Jamshed Khan <90842589+jamedkh@users.noreply.github.com>
2025-11-28 16:31:51 +0500

    chapters added

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  const userName = "Jamshed";
+  return (
+    <>
+      <h1 className="heading">React JS - Components ({userName})</h1>
+
+      <div className="card">
+        <h2>Mech 30</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
+          doloremque deleniti optio sit voluptatem est iure animi ipsam,
+          delectus sint?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ 02-components/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Card from "./components/card.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <section className="wrapper">
+      <App />
+      <Card />
+    </section>
+  </StrictMode>
+);
