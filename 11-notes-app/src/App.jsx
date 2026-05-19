4bde00ff3849a8e46123ad2daccd783d9fd6e17b
Jamshed Khan <jamedkh@gmail.com>
2026-05-19 18:31:33 +0500

    useState complete / Notes app start

--- 11-notes-app/src/App.jsx
+++ 11-notes-app/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     <>
       <section className="flex flex-col justify-center items-center w-full min-h-screen bg-gray-900 gap-4">
         <Heading
-          heading="React Js - 10 - useState Advanced"
+          heading="React Js - 11 - Notes App"
           className="text-4xl font-medium py-4 text-white"
         />
         <p className="text-2xl font-bold text-white">
